8615f33e592c8884031204a44b5255006c2167c2
abadanaf <abadanafchala@gmail.com>
2024-12-10 13:37:41 +0300

    the components files created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './app.css'
+import navbar from './Components/Navbar/navbar'
 const App = () => {
   return (
-    <h2>
-      App
-    </h2>
+    <>
+     <navbar/> 
+    </>
   )
 }
 
